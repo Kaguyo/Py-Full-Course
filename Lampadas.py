a353ebf55a24b2555b4837b8e909458ff963a007
Kaguyo <gabriel.silverio.silvaaa@gmail.comgit config --global user.name Gabriel>
2024-05-27 23:09:39 -0300

    Lamp

--- Lampadas.py
+++ Lampadas.py
@@ -5,7 +5,34 @@ def main():
 
     interruptor = input("Aperte interruptor 1 ou 2: ").split(" ")
     
-    for posicoes in interruptor:
+    for posicoes in (interruptor):
+        print(posicoes)
+        if posicoes == 1:
+            a += 1
+        else: a += 1; b += 1
+    
+    if a % 2 > 0 : print(1)
+    else: print(0)
+    if b % 2 > 0 : print(1)
+    else: print(0)
+
+if __name__ == "__main__":
+    main()
+
+# -/-
+
+def main():
+    n = int(input("Clicks no interruptor: "))
+    a = 0
+    b = 0
+
+    lista = []
+
+    for _ in range(n):
+        elemento = input("Digite um elemento: ")
+        lista.append(elemento)
+
+    for posicoes in lista:
         if posicoes == 1:
             a += 1
         else: a += 1; b += 1
