859681e1e0a0fb4da09a15320d8a935a43b00ce0
Kaguyo <gabriel.silverio.silvaaa@gmail.comgit config --global user.name Gabriel>
2024-05-29 02:03:15 -0300

    sccer

---
+++ Copa_Do_Mundo.py
@@ -0,0 +1,50 @@
+def main():
+    
+    Resultado_1 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 1: ",Resultado_1)
+
+    Resultado_2 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 2: ",Resultado_2)
+
+    Resultado_3 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 3: ",Resultado_3)
+
+    Resultado_4 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 4: ",Resultado_4)
+
+    Resultado_5 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 5: ",Resultado_5)
+
+    Resultado_6 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 6: ",Resultado_6)
+
+    Resultado_7 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 7: ",Resultado_7)
+
+    Resultado_8 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 8: ",Resultado_8)
+
+    Resultado_9 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 9: ",Resultado_9)
+
+    Resultado_10 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 10: ",Resultado_10)
+
+    Resultado_11 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 11: ",Resultado_11)
+
+    Resultado_12 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 12: ",Resultado_12)
+
+    Resultado_13 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 13: ",Resultado_13)
+
+    Resultado_14 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 14: ",Resultado_14)
+
+    Resultado_15 = input(str("Defina o placar: ")).split()
+    print("Placar jogo 15: ",Resultado_15)
+
+    
+if __name__ == "__main__":
+    main()
